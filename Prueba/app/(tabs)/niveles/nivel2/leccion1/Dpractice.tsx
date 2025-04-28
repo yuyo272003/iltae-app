786114d07cbe34364ce7cdb2a9a30397a5858bad
Gil Hernandez Ingrid Sarahi <zS22017036@estudiantes.uv.mx>
2025-04-28 15:04:21 -0600

    Avance nivel 2 leccion 1

---
+++ Prueba/app/(tabs)/niveles/nivel2/leccion1/Dpractice.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {router} from "expo-router";
+import PracticeImageAudioScreen from '../../../../../components/PracticeWordScreen';
+
+export default function PantallaLetra() {
+  const images = [
+    { id: '1', src: require('@/assets/images/lecciones/nivel2/pastel.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/pastel.wav'), isCorrect: false },
+    { id: '2', src: require('@/assets/images/lecciones/nivel2/balon.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/balon.wav'), isCorrect: true },
+    { id: '3', src: require('@/assets/images/lecciones/nivel1/lessons/dado.png'),audio: require('@/assets/audio/levels/nivel1/lessons/D/dado.wav'), isCorrect: false },
+    { id: '4', src: require('@/assets/images/lecciones/nivel1/lessons/dedo.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/dedo.wav'),isCorrect: true },
+    { id: '5', src: require('@/assets/images/lecciones/nivel2/miel.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/miel.wav'),isCorrect: false },
+    { id: '6', src: require('@/assets/images/lecciones/nivel2/diente.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/diente.wav'),isCorrect: true },
+  ];
+
+  return (
+    <PracticeImageAudioScreen
+        title="Dd"
+        titleAudio={require('@assets/audio/levels/nivel1/lessons/D/D.wav')}
+        practiceAudio={require('@/assets/audio/levels/nivel2/audios_lesson1/practice.wav')}
+        images={images}
+        //onCorrect={() => console.log('¡Correcto!')}
+        //onIncorrect={() => console.log('Intenta de nuevo')}
+        onTopBack={() => router.push('/(tabs)/Level2Screen')}
+        // @ts-ignore
+        onBottomBack={() => router.push('/(tabs)/niveles/nivel2/leccion1/Cpractice')}
+        // @ts-ignore
+        onNext={() => router.push('/(tabs)/niveles/nivel2/leccion1/Fpractice')}
+      
+    />
+  );
+}
---
+++ Prueba/app/(tabs)/niveles/nivel2/leccion1/Fpractice.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {router} from "expo-router";
+import PracticeImageAudioScreen from '../../../../../components/PracticeWordScreen';
+
+export default function PantallaLetra() {
+  const images = [
+    { id: '1', src: require('@/assets/images/lecciones/nivel2/foco.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/foco.wav'), isCorrect: false },
+    { id: '2', src: require('@/assets/images/lecciones/nivel2/balon.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/balon.wav'), isCorrect: true },
+    { id: '3', src: require('@/assets/images/lecciones/nivel2/fogata.png'),audio: require('@/assets/audio/levels/nivel2/audios_lesson1/fogata.wav'), isCorrect: false },
+    { id: '4', src: require('@/assets/images/lecciones/nivel1/lessons/fideos.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/fideos.wav'),isCorrect: true },
+    { id: '5', src: require('@/assets/images/lecciones/nivel2/falda.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/falda.wav'),isCorrect: false },
+    { id: '6', src: require('@/assets/images/lecciones/nivel2/dedo.png'), audio: require('@/assets/audio/levels/nivel2/audios_lesson1/dedo.wav'),isCorrect: true },
+  ];
+
+  return (
+    <PracticeImageAudioScreen
+        title="Ff"
+        titleAudio={require('@assets/audio/levels/nivel1/lessons/F/F.wav')}
+        practiceAudio={require('@/assets/audio/levels/nivel2/audios_lesson1/practice.wav')}
+        images={images}
+        //onCorrect={() => console.log('¡Correcto!')}
+        //onIncorrect={() => console.log('Intenta de nuevo')}
+        onTopBack={() => router.push('/(tabs)/Level2Screen')}
+        // @ts-ignore
+        onBottomBack={() => router.push('/(tabs)/niveles/nivel2/leccion1/Dpractice')}
+        // @ts-ignore
+        onNext={() => router.push('/(tabs)/niveles/nivel2/leccion1/Gpractice')}
+      
+    />
+  );
+}
