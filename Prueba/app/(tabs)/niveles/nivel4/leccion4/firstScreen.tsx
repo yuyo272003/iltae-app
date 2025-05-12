bfdfa620d88330390e4741173997721e1cab0eea
yuyo272003 <juliogm2003@hotmail.com>
2025-05-12 17:03:51 -0600

    Nivel 4 firstScreens

---
+++ Prueba/app/(tabs)/niveles/nivel4/leccion4/firstScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { router } from 'expo-router';
+import SyllableScreen from '../../../../../components/FirstSilabasMicrofono';
+
+export default function LeccionManzana() {
+    return (
+        <SyllableScreen
+            syllables={[
+                { text: 'Sol', audio: require('@assets/audio/Todos.wav') }
+            ]}
+            targetWord="Sol"
+            practiceAudio={require('@assets/audio/Todos.wav')}
+            // audios de retroalimentación
+            successAudio={require('@assets/audio/Todos.wav')}
+            failureAudio={require('@assets/audio/Todos.wav')}
+            imageSource={require('@assets/images/lecciones/nivel2/sol.png')}
+            onTopBack={() => router.push('/(tabs)/Level4Screen')}
+            // @ts-ignore
+            onNext={() => router.push('(tabs)/niveles/nivel4/leccion1/Sal')}
+        />
+    );
+}
